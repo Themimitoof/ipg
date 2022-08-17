73559870b4e82e53c8cdfb8a17cee2d645b1f1d4
Michael Vieira <contact@mvieira.fr>
2022-08-17 20:56:18 +0200

    Fix DNS record when using FQDN

--- src/dns.go
+++ src/dns.go
@@ -1,9 +1,16 @@
 package src
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Generate a DNS record ready to be paste on a Bind compatible zone
 func GenerateDNSRecord(ip string, ttl int, hostname string) string {
+	if strings.Contains(hostname, ".") && hostname[len(hostname)-1] != '.' {
+		hostname += "."
+	}
+
 	return fmt.Sprintf("%s\t%d\tIN\tAAAA\t%s", hostname, ttl, ip)
 }
 
